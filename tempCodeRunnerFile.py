fd3b2f505bdb2a90582f93bf26c6b99b62d6453e
jokernets <mrcoderpy@gmail.com>
2024-06-28 12:10:24 +0330

    First Commit

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+    #def progress(self):
+     #   self.prog.setValue(self.prog.value() + 1)
+        
+      #  while self.prog.value() == 100 :
+       #     self.close()
+            
+        #    self.openApp = Start()
+         #   self.openApp.show()
